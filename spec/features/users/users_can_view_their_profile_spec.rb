b8719f6a9cff0b671cb01e1c925e90a1601a1d07
Chris Smith <42@chrisryansmith.com>
2017-05-25 14:13:07 -0600

    make sure users are redirected when trying to view other's profiles

--- spec/features/users/users_can_view_their_profile_spec.rb
+++ spec/features/users/users_can_view_their_profile_spec.rb
@@ -14,5 +14,31 @@ RSpec.describe "a user can see their profile" do
     expect(page).to have_link "Edit Your Profile", href: edit_user_path(user)
   end
 
+  context "when trying to view another user's profile" do
+    it "shows them their own profile" do
+      this_user = create(:user)
+      other_user = create(:user)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(this_user)
+
+      visit user_path(other_user)
+
+      expect(page).to have_content "Your Profile"
+      expect(page).to have_content "Welcome, #{this_user.full_name}!"
+    end
+
+    it "redirects admins" do
+      this_user = create(:admin)
+      other_user = create(:user)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(this_user)
+
+      visit user_path(other_user)
+
+      expect(page).to have_content "Your Profile"
+      expect(page).to have_content "Welcome, #{this_user.full_name}!"
+    end
+  end
+
   it "a candidate can view additional candidate profile info"
 end
