1d4dd2c47044bfe2a78ea57863d1607c4fb08e77
Chris Smith <42@chrisryansmith.com>
2017-05-25 10:11:05 -0600

    add error messages to login form

--- spec/features/users/user_can_login_spec.rb
+++ spec/features/users/user_can_login_spec.rb
@@ -39,5 +39,60 @@ RSpec.describe "User logs in" do
   end
 
 
-  it "they see error messages for incorrect logins"
+  context "they see error messages for incorrect logins" do
+    it "no password" do
+      user = create(:user)
+
+      visit login_path
+      within("form") do
+        fill_in "Email", with: user.email
+        fill_in "Password", with: ''
+        click_on "Login"
+      end
+
+      expect(current_path).to eq login_path
+      expect(page).to have_content('Invalid email/password combination')
+    end
+
+    it "incorrect password" do
+      user = create(:user)
+
+      visit login_path
+      within("form") do
+        fill_in "Email", with: user.email
+        fill_in "Password", with: 'BadPass'
+        click_on "Login"
+      end
+
+      expect(current_path).to eq login_path
+      expect(page).to have_content('Invalid email/password combination')
+    end
+
+    it "no email" do
+      user = create(:user)
+
+      visit login_path
+      within("form") do
+        fill_in "Email", with: ''
+        fill_in "Password", with: user.password
+        click_on "Login"
+      end
+
+      expect(current_path).to eq login_path
+      expect(page).to have_content('Invalid email/password combination')
+    end
+
+    it "no user" do
+      visit login_path
+      within("form") do
+        fill_in "Email", with: 'made@up.com'
+        fill_in "Password", with: 'awesome!'
+        click_on "Login"
+      end
+
+      expect(current_path).to eq login_path
+      expect(page).to have_content('Invalid email/password combination')
+    end
+
+  end
 end
