582b8c8e1619967d46792b5edd60add6ecac0a14
Chris Smith <42@chrisryansmith.com>
2017-06-12 11:51:08 -0600

    add bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require popper
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require react
 //= require react_ujs
 //= require components
